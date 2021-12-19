2cd1332d6834f6df8d419d6ea43079dfbc70434e
Alan <alanmendoza057@gmail.com>
2021-12-18 22:07:02 -0600

    add font

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ fetch('../services.json',{
     data.map(servicio =>{
         
         template += 
-    `<div class="col-sm-12 col-md-4 mt-4">
+    `<div class="col-sm-12 col-md-4 my-4">
         <div class="card h-100">
             <img src="${servicio.img}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${servicio.name}</h5>
-              <p class="card-text ">${servicio.description}</p>
+              <p class="card-text " id="description">${servicio.description}</p>
               
               
             </div>
@@ -32,13 +32,15 @@ fetch('../services.json',{
                     </button>
                     </h2>
                     <div id="flush-collapse${servicio.id}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                    <div class="accordion-body">
+                    
+                    </div>
                     </div>
                     </div>
                 </div>
               </li>
             </ul>
-            <div class="card-footer">
+            <div class="card-footer text-center">
                 <a href="${servicio.link}" class="btn btn-primary">Ir al sitio</a>
              </div>
         </div>
